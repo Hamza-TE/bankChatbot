d531d5399b730f1ccf77fc4103fd829c22f66fc3
Hamza Jamal Shahid Khan <161300035+Hamza-TE@users.noreply.github.com>
2024-06-24 19:51:14 +0530

    added function calling feature

--- backend/utils/astraDB/astraDBsearch.js
+++ backend/utils/astraDB/astraDBsearch.js
@@ -11,11 +11,18 @@ async function astraDBsearch( inputText){
         const queryVector = await azureOpenAIEmbeddingConfig.configAzureEmbeddings.embedQuery(inputText);
         // Perform a similarity search
         results = await vectorStore.similaritySearchVectorWithScore(queryVector, 1);
+        // console.log('------------------search results',results);
+        const score = results[0][1];
+        console.log('------------------search score',score);
+        if (score < 0.90) {
+          return 'null';
+        } else {
+          return results[0][0].pageContent;
+        }
 } catch (error) {
     console.error('Error in AstraDB search request:', error.response ? error.response.data : error.message);
     throw error;
   };
-  return results[0][0].pageContent;
 };
 
 module.exports = {
